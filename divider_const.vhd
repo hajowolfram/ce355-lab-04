81bfc3e78d08707dc6b0e6aa213ba851b0d68a34
Hajo Wolfram <hajowolfram@Hajos-MacBook-Pro.local>
2025-10-18 22:59:18 -0500

    tb done

--- divider_const.vhd
+++ divider_const.vhd
@@ -4,4 +4,5 @@ use IEEE.std_logic_1164.all;
 package divider_const is
 	constant DIVIDEND_WIDTH : natural := 32;
 	constant DIVISOR_WIDTH : natural := 16;
+	constant CLK_PERIOD : time := 10 ns;
 end package divider_const;
--- divider_tb.vhd
+++ divider_tb.vhd
@@ -1,27 +1,28 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-
 library STD;
 use STD.textio.all;
 
 use WORK.divider_const.all;
+for all : divider use entity WORK.divider (behavioral_sequential);
 
 entity divider_tb is
 end entity divider_tb;
 
 architecture UsingTextFiles of divider_tb is
-    signal start : std_logic;
+    signal clk : std_logic := '0';
+    signal start : std_logic := '0';
     signal dividend : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
     signal divisor : std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
     signal quotient : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
     signal remainder : std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
-    signal overflow : std_logic;
+    signal overflow : std_logic := '0';
 
     component divider is 
         port(
             --Inputs
-            -- clk : in std_logic;
+            clk : in std_logic;
             --COMMENT OUT clk signal for Part A.
             start : in std_logic;
             dividend : in std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
@@ -36,6 +37,7 @@ architecture UsingTextFiles of divider_tb is
     
     dut: divider 
     port map (
+        clk => clk,
         start => start,
         dividend => dividend,
         divisor => divisor,
@@ -44,7 +46,17 @@ architecture UsingTextFiles of divider_tb is
         overflow => overflow
     );
 
-    process is
+    process 
+    begin
+        loop
+            clk <= '1';
+            wait for clk_period / 2;
+            clk <= '0';
+            wait for clk_period / 2;
+        end loop;
+    end process;
+
+    process
         file infile: text open read_mode is "divider32.in";
         file outfile: text open write_mode is "divider32.out";
 
@@ -62,12 +74,15 @@ architecture UsingTextFiles of divider_tb is
             read(line_in, a_i);
             readline(infile, line_in);
             read(line_in, b_i);
-            
+
+            wait until rising_edge(clk);
             dividend <= std_logic_vector(to_unsigned(a_i, DIVIDEND_WIDTH));
             divisor <= std_logic_vector(to_unsigned(b_i, DIVISOR_WIDTH));
             start <= '1';
-            wait for 100 ns;
-            
+            wait until rising_edge(clk);
+            start <= '0';
+            wait for 200 ns;
+            wait until rising_edge(clk);
             quotient_i := to_integer(unsigned(quotient));
             remainder_i := to_integer(unsigned(remainder));
             
@@ -85,7 +100,6 @@ architecture UsingTextFiles of divider_tb is
             
             writeline(outfile, line_out);
             
-            start <= '0';
             wait for 10 ns;
         end loop;
         wait;
